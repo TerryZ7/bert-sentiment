10c06a62372896e21cf119f488fe835b130723f3
TerryZ7 <49372575+TerryZ7@users.noreply.github.com>
2021-07-08 16:36:14 +0800

    Update train.py
    Add comments

--- train.py
+++ train.py
@@ -14,20 +14,32 @@ from transformers import get_linear_schedule_with_warmup
 
 
 def run():
+    # Transfer training set into csv
     dfx = pd.read_csv(config.TRAINING_FILE).fillna("none")
     dfx.sentiment = dfx.sentiment.apply(lambda x: 1 if x == "positive" else 0)
 
+    # Split training set into trainging set and validation set
+    ## test_size: Proportion of testing set
+    ## stratify: Make sure splits has the same proportion of value as the provided one
+    ### Output: List containing train-test split of inputs
     df_train, df_valid = model_selection.train_test_split(
         dfx, test_size=0.1, random_state=42, stratify=dfx.sentiment.values
     )
 
+    # Reset index of df_train to default one(0,1,2...) and drop the original index column
     df_train = df_train.reset_index(drop=True)
     df_valid = df_valid.reset_index(drop=True)
 
+    # Create a Custom Dataset
+    ## __init__, __len__, and __getitem__ are required as method
+    ## __init__ initialize features, labels. (and transform)
+    ## __len__ returns the number of sampels in dataset
+    ## __getitem__ return a sample given index
     train_dataset = dataset.BERTDataset(
         review=df_train.review.values, target=df_train.sentiment.values
     )
-
+    
+    ## Output: a iterator. next() is a list containng sample tensor and label tensor.
     train_data_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4
     )
@@ -39,11 +51,16 @@ def run():
     valid_data_loader = torch.utils.data.DataLoader(
         valid_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1
     )
-
+#########################################################################################
+    # Set device
     device = torch.device(config.DEVICE)
     model = BERTBaseUncased()
     model.to(device)
-
+    
+######################################################################################### 
+    # Optimizer
+    ## What do all these optimizer do? Why are there so many parameters to set?
+    ## Named_parameters return an iterator(yield both names and parameters)
     param_optimizer = list(model.named_parameters())
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
     optimizer_parameters = [
@@ -66,11 +83,14 @@ def run():
     scheduler = get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
     )
+    
+#########################################################################################
 
     best_accuracy = 0
     for epoch in range(config.EPOCHS):
         engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
         outputs, targets = engine.eval_fn(valid_data_loader, model, device)
+        # Note. True == 1, False == 0 in accuracy_scoere
         outputs = np.array(outputs) >= 0.5
         accuracy = metrics.accuracy_score(targets, outputs)
         print(f"Accuracy Score = {accuracy}")
